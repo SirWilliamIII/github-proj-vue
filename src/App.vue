c0de13a068511144223011c004eaf357e271bea6
Will Carpenter <willcarpenteriii@gmail.com>
2017-07-26 20:42:52 -0600

    dependecies installed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
-  </div>
+    <div>
+        whatup
+    </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-
-export default {
-  name: 'app',
-  components: {
-    Hello
-  }
-}
+	export default {
+		name:    'GitHub_App'
+	}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
